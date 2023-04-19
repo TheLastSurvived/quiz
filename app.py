a1dc8da25ae7000f8f0ff965d215f951a7f865b2
TheLastSurvived <mrsanja3@gmail.com>
2023-04-20 02:55:48 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,81 @@
+from flask import Flask, render_template, request, flash, redirect, session, url_for, abort,jsonify
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nails.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
+db = SQLAlchemy(app)
+
+class Quiz(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(100))
+    question_id = db.relationship('Question', backref='quiz', lazy='dynamic')
+
+    def __repr__(self):
+        return 'Quiz %r' % self.id 
+
+class Question(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    question = db.Column(db.String(100))
+    a = db.Column(db.String(100))
+    b = db.Column(db.String(100))
+    c = db.Column(db.String(100))
+    correct = db.Column(db.String(100))
+    id_quiz = db.Column(db.Integer, db.ForeignKey('quiz.id'))
+
+    def __repr__(self):
+        return 'Question %r' % self.id 
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    quiz = Quiz.query.all()
+    if request.method == 'POST':
+        title = request.form.get('title')
+        quiz = Quiz(title=title)
+        db.session.add(quiz)
+        db.session.commit()
+        return redirect(url_for("index"))
+    return render_template("index.html",quiz=quiz)
+
+@app.route('/education', methods=['GET', 'POST'])
+def education():
+    return render_template("education.html",quiz=quiz)
+
+@app.route('/delete-quiz/<int:id>', methods=['GET', 'POST'])
+def delete_quiz(id):
+    quiz = Quiz.query.get(id)
+    db.session.delete(quiz)
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/delete-question/<int:id>/<int:id_quiz>', methods=['GET', 'POST'])
+def delete_question(id,id_quiz):
+    question = Question.query.get(id)
+    db.session.delete(question)
+    db.session.commit()
+    return redirect(url_for("quiz", id=id_quiz))
+
+@app.route('/quiz/<int:id>', methods=['GET', 'POST'])
+def quiz(id):
+    quiz = Quiz.query.get(id)
+    question = Question.query.filter_by(id_quiz=id).all()
+    if request.method == 'POST':
+        question = request.form.get('question')
+        a = request.form.get('A')
+        b = request.form.get('B')
+        c = request.form.get('C')
+        correct = request.form.get('correct')
+        question = Question(question=question,a=a,b=b,c=c,correct=correct,id_quiz=id)
+        db.session.add(question)
+        db.session.commit()
+        return redirect(url_for("quiz", id=id))
+    return render_template("quiz.html",quiz=quiz,question=question)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/index.js
@@ -0,0 +1,62 @@
+
+
+const quiz = document.getElementById('quiz');
+const answerElements = document.querySelectorAll('.answer');
+const questionElement = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const submit = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz(){
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionElement.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+}
+
+function deselectAnswers(){
+    answerElements.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected(){
+    let answer;
+
+    answerElements.forEach(answerEl => {
+        if(answerEl.checked){
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+submit.addEventListener('click', () => {
+    const answer = getSelected();
+
+    if(answer){
+        if(answer === quizData[currentQuiz].correct){
+            score++;
+        }
+
+        currentQuiz++;
+
+        if(currentQuiz < quizData.length){
+            loadQuiz();
+        }
+        else{
+            quiz.innerHTML = `<div class="m-5 text-center"><h2>Вы правильно ответили на ${score}/${quizData.length} вопросов</h2>
+            <button onclick="location.reload()" class="btn btn-primary">Перезагрузить</button></div>
+            `;
+        }
+    }
+});
